76e3c0d1cfb87e83738ad04faeb8b33b29248ac5
Andy Susanto <officialandysusanto@gmail.com>
2023-08-21 10:17:07 +0700

    update

--- resources/views/filament/pages/biodata.blade.php
+++ resources/views/filament/pages/biodata.blade.php
@@ -1,4 +1,7 @@
 <x-filament::page>
+    Catatan : <span class="italic text-red-400">Data yang penting harus di isi adalah identitas diri dan alamat terutama
+        foto, Data Pendukung bersifat opsional bisa di inputkan di nanti . Data penelitian di tujukan untuk dosen /
+        tenaga pengajar</span>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
 
